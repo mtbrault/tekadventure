dc3b9713337828c557b90252ebffbaf759fdbe1a
Zakaria LAABID <zakaria.laabid@epitech.eu>
2017-04-21 23:36:25 +0200

    Placing some position

--- include/tekadv.h
+++ include/tekadv.h
@@ -5,7 +5,7 @@
 ** Login   <BlackBIrdz@epitech.net>
 ** 
 ** Started on  Fri Apr 21 03:27:45 2017 LAABID Zakaria
-** Last update Fri Apr 21 22:29:06 2017 Capitaine CASSE
+** Last update Fri Apr 21 23:19:48 2017 LAABID Zakaria
 */
 
 #ifndef TEKADV_H
@@ -41,9 +41,33 @@
 
 # define HELP		"USAGE:\t./tekadventure [file]\n\tfile : map config file.\n"
 
-# define MENU_PIC	(2)
+# define MENU_PIC	(13)
 # define STMENU_MAIN	"./ressources/menus/guimainmenu.png"
 # define STMENU_CHARAC	"./ressources/menus/guicharacter.png"
+
+# define MENU_NEWS	"./ressources/elements/nouveaute.png"
+# define MENU_NEWS_H	"./ressources/elements/nouveaute_hover.png"
+# define MENU_OPTION	"./ressources/elements/configuration.png"
+# define MENU_OPTION_H	"./ressources/elements/configuration_hover.png"
+# define MENU_SHOP	"./ressources/elements/sprite.png"
+# define MENU_SHOP_H	"./ressources/elements/sprite_hover.png"
+# define CHARAC_FECA	"./ressources/elements/feca.png"
+# define CHARAC_FECA_H	"./ressources/elements/feca_hover.png"
+# define CHARAC_IOP	"./ressources/elements/iop.png"
+# define CHARAC_IOP_H	"./ressources/elements/iop_hover.png"
+# define CHARAC_ECA	"./ressources/elements/eca.png"
+# define CHARAC_ECA_H	"./ressources/elements/eca_hover.png"
+# define CHARAC_OSA	"./ressources/elements/osamoda.png"
+# define CHARAC_OSA_H	"./ressources/elements/osamoda_hover.png"
+
+# define STMENU_PLAY	"./ressources/elements/jouer.png"
+# define STMENU_PLAY_H	"./ressources/elements/jouer_hover.png"
+# define STMENU_MULTI	"./ressources/elements/multijoueurs.png"
+# define STMENU_CLOSE	"./ressources/elements/close.png"
+# define STMENU_CLOSE_H	"./ressources/elements/close_hover.png"
+# define STMENU_START	"./ressources/elements/start.png"
+# define STMENU_START_H	"./ressources/elements/start_hover.png"
+
 # define MUSIC_MAIN	"./ressources/music/dofas_menu.ogg"
 # define MUSIC_CHARAC	"./ressources/music/dofas_character.ogg"
 
@@ -58,6 +82,7 @@ typedef struct		s_menu_conf
 {
   char	*tex_path;
   char	*music_path;
+  char	*hover_path;
 }			t_menu_conf;
 
 typedef struct		s_game
@@ -91,11 +116,14 @@ typedef struct		s_player
 typedef struct		s_menu
 {
   sfTexture		*tex;
+  sfTexture		*tex_h;
   sfSprite		*sprite;
-  sfUint8		*pixels;
+  sfSprite		*sprite_h;
   int			hover;
   sfVector2f		pos;
+  sfVector2f		pos_h;
   sfVector2u		size;
+  sfVector2u		size_h;
   sfMusic		*music;
 }			t_menu;
 
--- src/colors.c
+++ src/colors.c
@@ -5,7 +5,7 @@
 ** Login   <BlackBIrdz@epitech.net>
 ** 
 ** Started on  Fri Apr 21 04:12:09 2017 LAABID Zakaria
-** Last update Fri Apr 21 19:58:41 2017 Matthieu BRAULT
+** Last update Fri Apr 21 23:34:05 2017 LAABID Zakaria
 */
 
 #include <unistd.h>
@@ -15,8 +15,19 @@
 
 t_menu_conf	menu_conf[MENU_PIC]=
   {
-    {STMENU_MAIN, MUSIC_MAIN},
-    {STMENU_CHARAC, MUSIC_CHARAC}
+    {STMENU_MAIN, MUSIC_MAIN, NULL},
+    {STMENU_CHARAC, MUSIC_CHARAC, NULL},
+    {STMENU_PLAY, MUSIC_CHARAC, STMENU_PLAY_H},
+    {STMENU_MULTI, MUSIC_CHARAC, NULL},
+    {STMENU_CLOSE, MUSIC_CHARAC, STMENU_CLOSE_H},
+    {STMENU_START, MUSIC_CHARAC, STMENU_START_H},
+    {MENU_NEWS, MUSIC_CHARAC, MENU_NEWS_H},
+    {MENU_OPTION, MUSIC_CHARAC, MENU_OPTION_H},
+    {MENU_SHOP, MUSIC_CHARAC, MENU_SHOP_H},
+    {CHARAC_FECA, MUSIC_CHARAC, CHARAC_FECA_H},
+    {CHARAC_IOP, MUSIC_CHARAC, CHARAC_IOP_H},
+    {CHARAC_ECA, MUSIC_CHARAC, CHARAC_ECA_H},
+    {CHARAC_OSA, MUSIC_CHARAC, CHARAC_OSA_H},
   };
 
 void		music_change(int index, t_menu **menu)
@@ -25,9 +36,27 @@ void		music_change(int index, t_menu **menu)
   sfMusic_setLoop(menu[index]->music, sfTrue);
 }
 
+void	        set_position(t_menu **menu)
+{
+  sfVector2f	play;
+  sfVector2f	multi;
+  sfVector2f	close;
+
+  play = ((sfVector2f) {515.0, 250.0});
+  multi = ((sfVector2f) {515.0, 305.0});
+  close = ((sfVector2f) {515.0, 360.0});
+  sfSprite_setPosition(menu[2]->sprite, play);
+  sfSprite_setPosition(menu[3]->sprite, multi);
+  sfSprite_setPosition(menu[4]->sprite, close);
+}
+
 void		sprite_change(sfRenderWindow *window, int index, t_menu **menu)
 {
+  set_position(menu);
   sfRenderWindow_drawSprite(window, menu[index]->sprite, NULL);
+  sfRenderWindow_drawSprite(window, menu[2]->sprite, NULL);
+  sfRenderWindow_drawSprite(window, menu[3]->sprite, NULL);
+  sfRenderWindow_drawSprite(window, menu[4]->sprite, NULL);
 }
 
 t_menu		**gen_texture_space(t_menu **menu)
@@ -43,6 +72,17 @@ t_menu		**gen_texture_space(t_menu **menu)
   return (menu);
 }
 
+t_menu		**texture_init_hover(t_menu **menu, int i)
+{
+  menu[i]->tex_h = sfTexture_createFromFile(menu_conf[i].hover_path, NULL);
+  menu[i]->sprite_h = sfSprite_create();
+  menu[i]->pos_h = ((sfVector2f) {0.0, 0.0});
+  menu[i]->size_h = sfTexture_getSize(menu[i]->tex_h);
+  sfSprite_setPosition(menu[i]->sprite_h, menu[i]->pos_h);
+  sfSprite_setTexture(menu[i]->sprite_h, menu[i]->tex_h, sfTrue);
+  return (menu);
+}
+
 t_menu		**texture_init(t_menu **menu)
 {
   int		i;
@@ -50,6 +90,8 @@ t_menu		**texture_init(t_menu **menu)
   i = 0;
   while (i < MENU_PIC)
     {
+      if (menu_conf[i].hover_path != NULL)
+	menu = texture_init_hover(menu, i);
       menu[i]->tex = sfTexture_createFromFile(menu_conf[i].tex_path, NULL);
       menu[i]->sprite = sfSprite_create();
       menu[i]->pos = ((sfVector2f) {0.0, 0.0});
