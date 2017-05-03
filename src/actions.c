1a933e0ff218abbc9237aa837a79a3e8987648f1
Capitaine CASSE <antoine.casse@epitech.eu>
2017-05-03 19:57:59 +0200

    début pathfinding

--- src/actions.c
+++ src/actions.c
@@ -5,7 +5,7 @@
 ** Login   <antoine.casse@epitech.net>
 ** 
 ** Started on  Sat Apr 22 19:50:07 2017 Capitaine CASSE
-** Last update Wed May  3 11:52:04 2017 Capitaine CASSE
+** Last update Wed May  3 18:57:00 2017 Capitaine CASSE
 */
 
 #include "tekadv.h"
@@ -19,14 +19,14 @@ sfVector2f	get_mult_size(sfVector2i size, sfVector2i tile)
   return (res);
 }
 
-sfVector2i	convert_pos(sfVector2i pos, sfVector2i tile)
+sfVector2f	convert_pos(sfVector2i pos, sfVector2i tile)
 {
-  sfVector2i	res;
+  sfVector2f	res;
 
-  res.x = SCR_W / 2;
-  res.y = 0;
-  res.x += pos.x * tile.x / 2 - pos.y * tile.y / 2;
-  res.y += pos.x * tile.x / 2 + (pos.y + 1) * tile.y / 2;
+  res.x = (float)SCR_W / 2;
+  res.y = 0.0;
+  res.x += (float)(pos.x * tile.x / 2 - pos.y * tile.y / 2);
+  res.y += (float)(pos.x * tile.x / 2 + (pos.y + 1) * tile.y / 2);
   return (res);
 }
 
--- src/pathfind.c
+++ src/pathfind.c
@@ -5,86 +5,54 @@
 ** Login   <antoine.casse@epitech.net>
 ** 
 ** Started on  Tue May  2 22:22:39 2017 Capitaine CASSE
-** Last update Wed May  3 13:10:48 2017 Capitaine CASSE
+** Last update Wed May  3 19:56:49 2017 Capitaine CASSE
 */
 
 #include "tekadv.h"
 
-sfVector2i		dirtab[4] = {
-  (sfVector2i){0, 1},
-  (sfVector2i){0, -1},
-  (sfVector2i){1, 0},
-  (sfVector2i){-1, 0},
-};
-
-static int	find_star(sfVector2i *next, sfVector2i pos,
-				  sfVector2i dest, int **map)
-{
-  if (dest.x <= pos.x)
-    dest.x = 1;
-  else
-    dest.x = 1;
-  if (dest.x <= pos.x)
-     dest.x = 1;
-   else
-     dest.x = 1;
-  return (0);
-}
-
-static sfVector2i	find_path(int **map, sfVector2i pos, sfVector2i dest)
-{
-  sfVector2i		next;
-  sfVector2i		tmp;
-
-  while (!find_star(&next, pos, dest, map))
-    {
-      tmp = find_path(map, next, dest);
-      if (tmp.y != -1 && tmp.x != -1)
-	return (tmp);
-    }
-  next.x = -1;
-  next.y = -1;
-  return (next);
-}
-
 static int	check_click(int *state, sfVector2i *dir,
 			    sfRenderWindow *window, t_game *game)
 {
-  static int	x = -1;
-  static int	y = -1;
+  static sfVector2i	vec = {-1, -1};
 
-  if (x == game->player->pos.x && y == game->player->pos.y)
+  if (vec.x == game->player->pos.x && vec.y == game->player->pos.y)
+    vec = {-1, -1};
+  else if (vec.x != -1 && vec.y != -1)
     {
-      x = -1;
-      y = -1;
+      *dir = vec;
+      return (1);
     }
   if (sfMouse_isButtonPressed(sfMouseRight))
     {
       *dir = raw_click(game, window);
-      x = (*dir).x;
-      y = (*dir).y;
+      vec = (*dir);
       return (1);
     }
-  else if (x != -1 && y != -1)
+  else if (vec.x != -1 && vec.y != -1)
     return (1);
   return (0);
 }
 
-sfVector2f	find_pos(t_player *ply, t_game *game, sfRenderWindow *window)
+int	check_obstacles(sfVector2i pos, sfVector2i *dir)
+{
+  return (0);
+}
+
+int	find_pos(t_player *ply, t_game *game, sfRenderWindow *window)
 {
   sfVector2i	dir;
   sfVector2f	res;
+  sfVector2f	dest;
   static int	state = 0;
 
   res.x = 0.0;
   res.y = 0.0;
   if (state || check_click(&state, &dir, window, game))
-    return (res);
-  dir = find_path(game->map[0], ply->pos, dir);
-  if (dir.x == -1 && dir.y == 1)
-    return (res);
-  dir.x -= ply->pos.x;
-  dir.y -= ply->pos.y;
-  
-  return (res);
+    return (0);
+  dest = convert_pos(dir, game->tile);
+  res.x = dest - (float)ply->pos.x;
+  res.y = dest - (float)ply->pos.y;
+  if (move_perso(res, ply, window))
+    state = 0;
+  return (0);
 }
