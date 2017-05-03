d35fefbbcc951410a249b0d6d35abc55a82a6645
Matthieu <matthieu.brault@epitech.eu>
2017-05-03 19:19:10 +0200

    fonction qui pue la merde pour l'animation : antoine amuse toi bien à debug lol mdr

--- src/anim.c
+++ src/anim.c
@@ -5,37 +5,75 @@
 ** Login   <b00bix@epitech.net>
 ** 
 ** Started on  Wed May  3 16:03:43 2017 Matthieu BRAULT
-** Last update Wed May  3 18:42:53 2017 Matthieu BRAULT
+** Last update Wed May  3 19:18:31 2017 Matthieu BRAULT
 */
 
 #include <math.h>
 #include "tekadv.h"
 
+/* void		move_perso(sfVector2f vector, t_player *p, sfRenderWindow *window) */
+/* { */
+/*   sfVector2f	move; */
+/*   sfVector2f	axe; */
+/*   sfSprite	*sprite; */
+/*   int		len; */
+/*   int		i; */
+
+/*   if (vector.x == 0 && vector.y == 0) */
+/*     { */
+/*       sprite = get_static_char(p->classe, ((sfVector2i) */
+/* 	{p->dir, p->class}), p->pos, ((sfVector2i) {8, 4})); */
+/*       sfRenderWindow_drawSprite(window, sprite, NULL); */
+/*       return ; */
+/*     } */
+/*   axe.x = vector.x / sqrt(pow(vector.x, 2) + pow(vector.y, 2)); */
+/*   axe.y = vector.y / sqrt(pow(vector.x, 2) + pow(vector.y, 2)); */
+/*   len = sqrt(pow(vector.x, 2) + pow(vector.y, 2)) / axe.x; */
+/*   i = 0; */
+/*   move = ((sfVector2f) {0, 0}); */
+/*   while (i <= len) */
+/*     { */
+/*       sprite = get_static_char(p->spriteboard, ((sfVector2i) */
+/* 	{p->dir, p->class}), ((sfVector2f) {(float)p->pos.x + move.x, */
+/* 	      p->pos.y + move.y}), ((sfVector2i) {6, 8})); */
+/*       sfRenderWindow_drawSprite */
+/*       move = ((sfVector2f) {move.x + axe.x, move.y + axe.y}); */
+/*       i = i + 1; */
+/*     } */
+/* } */
+
 void		move_perso(sfVector2f vector, t_player *p, sfRenderWindow *window)
 {
   static sfVector2f	move = ((sfVector2f) {0, 0});
   static sfVector2f	axe;
+  static sfVector2i	i = ((sfVector2i) {0, 0});
+  static sfVector2i	loop == ((sfVector2i) {0, 0});
   sfSprite		*sprite;
 
   if (vector.x == 0 && vector.y == 0)
     {
       sprite = get_static_char(p->classe, ((sfVector2i)
-	{p->dir, p->class}), player->pos, ((sfVector2i) {8, 4}));
+	{p->dir, p->class}), p->pos, ((sfVector2i) {8, 4}));
       sfRenderWindow_drawSprite(window, sprite, NULL);
       return ;
     }
-  if (move == ((sfVector2f) {0, 0}) || move == vector)
+  else if (i.x == 0)
     {
       axe.x = vector.x / sqrt(pow(vector.x, 2) + pow(vector.y, 2));
       axe.y = vector.y / sqrt(pow(vector.x, 2) + pow(vector.y, 2));
+      i.x = sqrt(pow(vector.x, 2) + pow(vector.y, 2)) / vector.x;
     }
   move = ((sfVector2f) {move.x + axe.x, move.y + axe.y});
   sprite = get_static_char(p->spriteboard, ((sfVector2i)
-    {p->dir, p->class}), p->pos + move, ((sfVector2i) {6, 8}));
+    {loop.y, p->dir}), p->pos + move, ((sfVector2i) {6, 8}));
   sfRenderWindow_drawSprite(window, sprite, NULL);
-  if (move == vector)
+  loop.x = loop.x + 1;
+  if (loop.y % 100 == 0)
+    loop.y = loop.y + 1;
+  if (i.y > i.x)
     {
-      p->pos = p->pos + move;
+      i = ((sfVector2i) {0, 0});
+      loop = ((sfVector2i) {0, 0});
       move = ((sfVector2f) {0, 0});
     }
 }
