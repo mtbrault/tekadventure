d7b7c059c985a0fbad6160043d3b3857ed054ca5
Matthieu <matthieu.brault@epitech.eu>
2017-05-03 18:42:55 +0200

    re

--- src/anim.c
+++ src/anim.c
@@ -5,7 +5,7 @@
 ** Login   <b00bix@epitech.net>
 ** 
 ** Started on  Wed May  3 16:03:43 2017 Matthieu BRAULT
-** Last update Wed May  3 18:22:54 2017 Matthieu BRAULT
+** Last update Wed May  3 18:42:53 2017 Matthieu BRAULT
 */
 
 #include <math.h>
@@ -26,8 +26,6 @@ void		move_perso(sfVector2f vector, t_player *p, sfRenderWindow *window)
     }
   if (move == ((sfVector2f) {0, 0}) || move == vector)
     {
-      p->pos = p->pos + move;
-      move = ((sfVector2f) {0, 0});
       axe.x = vector.x / sqrt(pow(vector.x, 2) + pow(vector.y, 2));
       axe.y = vector.y / sqrt(pow(vector.x, 2) + pow(vector.y, 2));
     }
@@ -35,4 +33,9 @@ void		move_perso(sfVector2f vector, t_player *p, sfRenderWindow *window)
   sprite = get_static_char(p->spriteboard, ((sfVector2i)
     {p->dir, p->class}), p->pos + move, ((sfVector2i) {6, 8}));
   sfRenderWindow_drawSprite(window, sprite, NULL);
+  if (move == vector)
+    {
+      p->pos = p->pos + move;
+      move = ((sfVector2f) {0, 0});
+    }
 }
